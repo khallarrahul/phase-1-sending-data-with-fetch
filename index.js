8c044cff9417f25697be96a6c41e38eab1cba87b
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-04-12 14:50:16 -0700

    adds sending data

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Add your code here
+// 
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   function updateDogs(object) {
+//     fetch("http://localhost:3000/dogs", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//       },
+//       body: JSON.stringify(object),
+//     })
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (object) {
+//         console.log(object);
+//       })
+//       .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
+//   }
+
+
+function submitData(name, email){
+    const bioData = {
+        name : name,
+        email : email
+    }
+    return fetch("http://localhost:3000/users", {
+        method:"POST",
+        headers:{
+            "Content-Type" : "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(bioData)
+    })
+    .then(res => res.json())
+    .then(function(data){document.body.innerHTML = data.id})
+    .catch(function(error){
+        alert("Bad request");
+        document.body.innerHTML += error.message
+    })
+}
+console.log(data)
+
+  
